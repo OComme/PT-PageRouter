5e2586786ad0bfc0d05ccca79f33eb0ba39147d7
Blanbok <a1653913692@icloud.com>
2017-12-17 21:56:20 +0800

    【Add】Project

---
+++ PT-PrefixHeaderDemo/PT-PrefixHeaderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PT-PrefixHeaderDemo
+//
+//  Created by BlanBok on 2017/12/17.
+//  Copyright © 2017年 OComme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PT-PrefixHeaderDemo/PT-PrefixHeaderDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PT-PrefixHeaderDemo
+//
+//  Created by BlanBok on 2017/12/17.
+//  Copyright © 2017年 OComme. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
